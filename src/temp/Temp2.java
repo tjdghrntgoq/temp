db82b91f24d9fc4e03feb264a01ef911a0cd76e8
choisungho <tjdghrntgoq@naver.com>
2022-05-31 10:30:31 +0900

    2nd

--- src/temp/Temp2.java
+++ src/temp/Temp2.java
@@ -1,11 +1,10 @@
 package temp;
 
-public class TEMP {
+public class Temp2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("temp");
-		System.out.println("temp");
+
 	}
 
 }
